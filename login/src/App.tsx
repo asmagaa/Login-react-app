4eb54bac4d51030aab133405115aa735c0d5c995
Aleksander Smaga <165085833+asmagaa@users.noreply.github.com>
2025-01-24 23:12:04 +0100

    Final fix
    ...

--- login/src/App.tsx
+++ login/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom' // Use NavLink
 import Preferences from './components/Preferences'
 import ChangePassword from './components/Password'
 import AccountSettings from './components/Account'
@@ -8,16 +8,26 @@ import './App.css'
 const App: React.FC = () => {
 	return (
 		<Router>
-			<nav>
-				<Link to='/account-settings'>Account Settings</Link>
-				<Link to='/change-password'>Change Password</Link>
-				<Link to='/preferences'>Preferences</Link>
-			</nav>
-			<Routes>
-				<Route path='/account-settings' element={<AccountSettings />} />
-				<Route path='/change-password' element={<ChangePassword />} />
-				<Route path='/preferences' element={<Preferences />} />
-			</Routes>
+			<div className='panel-container'>
+				<nav>
+					<NavLink to='/account-settings' className={({ isActive }) => (isActive ? 'active' : '')}>
+						Account Settings
+					</NavLink>
+					<NavLink to='/change-password' className={({ isActive }) => (isActive ? 'active' : '')}>
+						Change Password
+					</NavLink>
+					<NavLink to='/preferences' className={({ isActive }) => (isActive ? 'active' : '')}>
+						Preferences
+					</NavLink>
+				</nav>
+				<div className='main-content'>
+					<Routes>
+						<Route path='/account-settings' element={<AccountSettings />} />
+						<Route path='/change-password' element={<ChangePassword />} />
+						<Route path='/preferences' element={<Preferences />} />
+					</Routes>
+				</div>
+			</div>
 		</Router>
 	)
 }
